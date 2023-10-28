dbe6c0ae6b597c83e25f5176df8d8ca6a22b07f8
wishwaD <dissanayakewishwajith@gmail.com>
2023-10-29 02:25:43 +0530

    change addons

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -29,7 +29,7 @@ export default function Task({
           readOnly={true}
           name="title"
           placeholder="Input title"
-          style={{ background: "red" }}
+          style={{ textOverflow: "ellipsis" }}
         />
       </label>
 
