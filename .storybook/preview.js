2bb7ada8c37b0aa8448a1161d60e844f07db42bd
wishwaD <dissanayakewishwajith@gmail.com>
2023-10-29 00:05:30 +0530

    create simple and composite component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import "../src/index.css";
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
